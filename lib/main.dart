a136871ca162998cfd4c85d4705fb31f5147c820
mohas8 <mdmobashirhasan@gmail.com>
2025-02-18 11:22:09 +0600

    gradual_icon increase

--- lib/main.dart
+++ lib/main.dart
@@ -25,46 +25,23 @@ class LabClass05 extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Basic Flutter Design -02"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Hi Whats'up?", 
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 20), 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.abc_outlined,  
-                size: 40,
-                color: Colors.green,
-              ),
-              Icon(
-                Icons.access_alarm,  
-                size: 40,
+            children: List.generate(
+              100,
+              (index) => Icon(
+                Icons.star, 
+                size: 10 + index.toDouble(),
                 color: Colors.yellow,
               ),
-              Icon(
-                Icons.access_alarm_outlined,  
-                size: 40,
-                color: Colors.red,
-              ),
-              Icon(
-                Icons.access_time_filled_outlined,  
-                size: 40,
-                color: Colors.black,
-              ),
-              Icon(
-                Icons.star_border, // ok
-                size: 40,
-                color: Colors.black,
-              ),
-            ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
