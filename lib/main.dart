d09594196a236584b810b48ac6cf0853d7f340bb
mohas8 <mdmobashirhasan@gmail.com>
2025-02-18 10:24:39 +0600

    Basic Flutter UI - 02

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const LabClass05());
 }
 
-class MyApp extends StatelessWidget {
+class LabClass05 extends StatelessWidget {
+  const LabClass05({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Rating App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _rating = 0;
-
-  void _setRating(int rating) {
-    setState(() {
-      _rating = rating;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('The First App'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(5, (index) {
-              return IconButton(
-                icon: Icon(
-                  index < _rating ? Icons.star : Icons.star_border,
-                  color: const Color.fromARGB(255, 0, 0, 0),
-                ),
-                onPressed: () => _setRating(index + 1),
-              );
-            }),
-          ),
-          Text(
-            'Rating: $_rating',
-            style: TextStyle(fontSize: 24),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              // Add your submit code here!
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text('Submitted! Rating: $_rating')),
-              );
-            },
-            child: Text('Submit'),
-          ),
-        ],
+      title: "Basic Flutter UI - 02",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Basic Flutter UI - 02"),
+          backgroundColor: Colors.blue[400],
+          centerTitle: true,
+        ),
+        body: Row(
+          children: [
+            Text(
+              "Hi, ",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              "This is ",
+              style: TextStyle(
+                color: Colors.yellow,
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              "Flutter.",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 10,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
