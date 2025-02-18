d96e74e35900172c3a4dd73cdddaa4057b977ebc
mohas8 <mdmobashirhasan@gmail.com>
2025-02-18 11:02:40 +0600

    spaceEvenly

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const LabClass05());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Basic UI -02",
+      home: LabClass05(),
+    );
+  }
 }
 
 class LabClass05 extends StatelessWidget {
@@ -9,39 +21,50 @@ class LabClass05 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Basic Flutter UI - 02",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Basic Flutter UI - 02"),
-          backgroundColor: Colors.blue[400],
-          centerTitle: true,
-        ),
-        body: Row(
-          children: [
-            Text(
-              "Hi, ",
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Basic Flutter Design -02"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Hi Whats'up?", 
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20), 
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.abc_outlined,  
+                size: 40,
+                color: Colors.green,
               ),
-            ),
-            Text(
-              "This is ",
-              style: TextStyle(
+              Icon(
+                Icons.access_alarm,  
+                size: 40,
                 color: Colors.yellow,
-                fontSize: 30,
               ),
-            ),
-            Text(
-              "Flutter.",
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 10,
+              Icon(
+                Icons.access_alarm_outlined,  
+                size: 40,
+                color: Colors.red,
               ),
-            ),
-          ],
-        ),
+              Icon(
+                Icons.access_time_filled_outlined,  
+                size: 40,
+                color: Colors.black,
+              ),
+              Icon(
+                Icons.star_border, // ok
+                size: 40,
+                color: Colors.black,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
