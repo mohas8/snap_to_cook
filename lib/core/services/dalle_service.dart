e64dff6b7a9256ec90657a404418432328098446
tanvir5386 <islam15-5386@diu.edu.bd>
2025-03-24 15:06:25 +0600

    new

--- lib/core/services/dalle_service.dart
+++ lib/core/services/dalle_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+// ignore: depend_on_referenced_packages
+import "package:http/http.dart" as http;
 import '../constants/app_constants.dart';
 
 class DalleService {
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:snap_to_cook/routes/voice_input_screen.dart';
+import '../features/home/screens/home_screen.dart';
+import '../features/input_methods/screens/image_input_screen.dart';
+import '../features/input_methods/screens/voice_input_screen.dart';
+import '../features/input_methods/screens/text_input_screen.dart';
+import '../features/meal_planner/screens/planner_screen.dart';
+import '../features/preferences/screens/dietary_preferences_screen.dart';
+import '../features/shopping_list/screens/shopping_list_screen.dart';
+
+class AppRoutes {
+  static const String home = '/';
+  static const String imageInput = '/image-input';
+  static const String voiceInput = '/voice-input';
+  static const String textInput = '/text-input';
+  static const String planner = '/planner';
+  static const String dietaryPreferences = '/preferences';
+  static const String shoppingList = '/shopping-list';
+
+  static Map<String, WidgetBuilder> routes = {
+    home: (context) => HomeScreen(),
+    imageInput: (context) => ImageInputScreen(),
+    voiceInput: (context) {
+      var voiceInputScreen = VoiceInputScreen();
+      return newMethod(voiceInputScreen);
+    },
+    textInput: (context) => TextInputScreen(),
+    planner: (context) => PlannerScreen(),
+    dietaryPreferences: (context) => DietaryPreferencesScreen(),
+    shoppingList: (context) => ShoppingListScreen(),
+  };
+
+  static VoiceInputScreen newMethod(VoiceInputScreen voiceInputScreen) => voiceInputScreen;
+}
